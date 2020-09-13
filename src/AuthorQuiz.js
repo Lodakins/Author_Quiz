d38fb4a966772329daf1834bf3b9b1e37b3fec08
Akinrolabu Lolade <l.akinrolabu@csdcconsulting.com>
2020-09-13 11:52:31 +0100

    Worked on the event listener of app

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import PropTypes from 'prop-types';
 import './App.css';
 import './bootstrap.min.css';
 import './authorquiz.css';
@@ -10,8 +11,9 @@ import './authorquiz.css';
 function Header(){
     return(
            <div className="row">
-              <header>
+              <header className="jumbotron col-md-12">
                   <h3 className="text-center text-primary"> Author Quiz</h3>
+                  <p> Please select book written by the author</p>
               </header>
            </div>
     )
@@ -27,25 +29,57 @@ function Footer(){
   )
 }
 
-function MainBody(){
+function MainBody({author,books,highlight,checkAnswer}){
+
+  function getBackgroundColor(highlight){
+    let mapping={
+        "none":"",
+        "correct":"green",
+        "incorrect":"red"
+    }
+
+    return mapping[highlight];
+
+  }
+
   return(
-      <div className="row">
-          <div className="col-md-4 offset-1">
-                <p> This is for images</p>
+      <div className="row mb-5" style={{backgroundColor:getBackgroundColor(highlight) ,padding:"15px"}}>
+          <div className="col-md-5">
+                <img src={author.imageUrl} className="authorImage" alt="author" />
           </div>
           <div className="col-md-6">
-                <p> This is for the Quiz</p>
+            {books.map(title=> <Book title={title}  key={title} onClick={checkAnswer} />)}
           </div>
       </div>
   ) 
 
 }
 
-function AuthorQuiz() {
+function Book({title,onClick}){
+  return(
+  <div style={{backgroundColor:"#57df90",padding:"20px 30px",marginTop:"20px"}} onClick={()=>onClick(title)}> <h4> {title} </h4></div>
+  )
+}
+
+
+MainBody.propTypes={
+    author: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imageUrl: PropTypes.string.isRequired,
+        imageSource:PropTypes.string.isRequired,
+        books: PropTypes.arrayOf(PropTypes.string).isRequired
+    }),
+    books: PropTypes.string.isRequired,
+    checkAnswer: PropTypes.func.isRequired,
+    highlight: PropTypes.string.isRequired
+}
+
+
+function AuthorQuiz({turndata,highlight,checkAnswer}) {
   return (
     <div className="container-fluid">
         <Header />
-        <MainBody />
+        <MainBody  {...turndata} highlight={highlight} checkAnswer={checkAnswer}/>
         <Footer />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,18 +2,109 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
+import Click from './click';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
 
-let state={
-    
+const authors=[
+  {
+    name: 'Mark Twain',
+    imageUrl: 'images/authors/marktwain.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['The Adventures of Huckleberry Finn']
+  },
+  {
+    name: 'Joseph Conrad',
+    imageUrl: 'images/authors/josephconrad.png',
+    imageSource: 'Wikimedia Commons',
+    books: ['Heart of Darkness']
+  },
+  {
+    name: 'J.K. Rowling',
+    imageUrl: 'images/authors/jkrowling.jpg',
+    imageSource: 'Wikimedia Commons',
+    imageAttribution: 'Daniel Ogren',
+    books: ['Harry Potter and the Sorcerers Stone']
+  },
+  {
+    name: 'Stephen King',
+    imageUrl: 'images/authors/stephenking.jpg',
+    imageSource: 'Wikimedia Commons',
+    imageAttribution: 'Pinguino',
+    books: ['The Shining', 'IT']
+  },
+  {
+    name: 'Charles Dickens',
+    imageUrl: 'images/authors/charlesdickens.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['David Copperfield', 'A Tale of Two Cities']
+  },
+  {
+    name: 'William Shakespeare',
+    imageUrl: 'images/authors/williamshakespeare.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+  }
+]
+
+const state={
+    turndata:getTurnData(authors),
+    author:getauthors(authors),
+    highlight:""
 }
+
+function getauthors(auth){
+    const allAuthors = auth.reduce(function(c,p,i){
+         c.push(p.name);
+         return c;
+    },[]);
+
+    return allAuthors;
+}
+
+
+function getTurnData(auth){
+
+   const allBooks = auth.reduce(function(c,p,i){
+
+        let arr = [...c, ...p.books]
+       
+        return arr;
+        
+   },[])
+
+   console.log("Books array: "+ allBooks);
+
+   const fourRandomBooks = shuffle(allBooks).slice(0,4);
+   const answer= sample(fourRandomBooks);
+    return {
+       books:fourRandomBooks,
+       author: auth.find((authors)=>authors.books.some((title)=>title === answer))
+   }
+}
+
+console.log(state)
+function handle(val){
+    console.log(`${val} clicked.`);
+}
+function checkAnswer(title){
+    let isCorrect= state.turndata.author.books.some((item)=> item === title);
+    state.highlight = isCorrect ? "correct" :"incorrect";
+    render();
+}
+
+function render(){
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthorQuiz />
+    <AuthorQuiz  {...state} checkAnswer={checkAnswer}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
